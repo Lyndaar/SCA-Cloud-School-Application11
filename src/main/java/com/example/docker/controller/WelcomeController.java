602d982c826f7a4f5a59a9b26509ece5f93f2ecd
Lyndaar <uzadaku96@gmail>
2021-01-15 03:11:19 +0100

    first commit, added all files

---
+++ src/main/java/com/example/docker/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String WelcomeSCA(){
+        return "Welcome to SCA Cloud School Application";
+    }
+}
